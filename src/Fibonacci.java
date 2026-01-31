6bb1f5d1cf5c23a9ba5563cb0077c047c5ec3c5f
govind <govind.g.coder@gmail.com>
2026-01-31 14:59:08 +0530

    initial commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Fibonacci {
-    
-        // TODO: Read N
-        // TODO: Handle base cases (N=1 -> 0, N=2 -> 1)
-        // TODO: Calculate the Nth number for N > 2
-        // TODO: Print the result
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        if (N == 0) {
+            System.out.print(0);
+            return;
+        } else if (N == 1) {
+            System.out.print(0);
+            return;
+        } else if (N == 2) {
+            System.out.print(1);
+            return;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 3; i <= N; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        System.out.print(b);
+    }
 }
